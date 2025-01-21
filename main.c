aeefe3465c88657975891d7d74df14eb9a7dbd27
NiranjKumar <165467829+NiranjKumar@users.noreply.github.com>
2025-01-22 00:02:33 +0530

    updated main.c

--- main.c
+++ main.c
@@ -6,8 +6,6 @@
 #include "rtc.h"
 #include "uart.h"
 
-
-
 int main()
 {
 	unsigned char hr,min,sec,con1,con2;
@@ -19,14 +17,15 @@ int main()
 	lcd_command(0x80);
 	lcd_str("   PCB DAQ");
 	delay_ms(2000);
-	i2c_rtc_write(0x68,0x00,0x50);
-	i2c_rtc_write(0x68,0x01,0x59);
-	i2c_rtc_write(0x68,0x02,0x71);
+	i2c_rtc_write(0x68,0x00,0x59);
+	i2c_rtc_write(0x68,0x01,0x20);
+	i2c_rtc_write(0x68,0x02,0x63);
 	lcd_command(0x01);
 	while(1)
 	{
 		lcd_command(0x80);
 		lcd_str("Time:");
+		uart_str("Time:");
 		
 		con1=i2c_rtc_read(0X68,0X02);
 		con2=con1 & 0x1f;
@@ -34,30 +33,47 @@ int main()
 		
 		lcd_data((hr/10)+48);
 		lcd_data((hr%10)+48);
+		uart_tx((hr/10)+48);
+		uart_tx((hr%10)+48);
+		uart_tx(':');
+
 		lcd_data(':');
 		
 		min=bcd_conv(i2c_rtc_read(0X68,0X01));
 		lcd_data((min/10)+48);
 		lcd_data((min%10)+48);
+		uart_tx((min/10)+48);
+		uart_tx((min%10)+48);
+		uart_tx(':');
 		lcd_data(':');
 	  
 		sec=bcd_conv(i2c_rtc_read(0X68,0X00));
 		lcd_data((sec/10)+48);
 		lcd_data((sec%10)+48);
+		uart_tx((sec/10)+48);
+		uart_tx((sec%10)+48);
+
 		if((con1>>5)&1==1)
 		{
 			lcd_str(" PM");
+			uart_str(" PM");
 		}
 		else
 		{
 			lcd_str(" AM");
+			uart_str(" AM");
 		}
 		
 		f=read_adc(0);
-		f=f*10;
+		f=f*100;
 		lcd_command(0xc0);
+		uart_str("\r\n");
+		lcd_str("Temp: ");
+		uart_str("Temp: ");
 		lcd_float(f);
-	
+		delay_ms(200);
+		uart_float(f);
+		uart_str("\r\n");
 	}
 	
 }
